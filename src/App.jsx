8eb3bc9cd22bb3993f38123aed9ba59c41d64d7e
mohmmadAyesh <mohmmadayesh22@gmail.com>
2025-04-19 11:40:08 +0200

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// App.js
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<div>
+
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button className='btn'
+					onClick={() =>
+						setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+			</div>
+		</>
+	)
+}
+
+export default App
